0b780b44f940d37d6c921d1a19d75a0e17d48a3e
Augustus <453155399@qq.com>
2016-03-21 17:12:42 +0800

    完成验证

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['wx'] = 'wx/BaseController/valid';
+$route['wx'] = 'wx/BaseController/responseMsg';
