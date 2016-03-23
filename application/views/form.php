43c069590d4815a1e7467619ba3d857bf94b3e00
AugustusWang1028 <wangdw.augustus@qq.com>
2016-03-23 18:21:07 +0800

    报名表后台

--- application/views/form.php
+++ application/views/form.php
@@ -5,3 +5,13 @@
  * Date: 2016-03-23
  * Time: 17:57
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>403 Forbidden</title>
+</head>
+<body>
+<p>理论上这里是一张报名表</p>
+</body>
+</html>
