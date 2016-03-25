f15983f0460c115fa2f46371464b6cb159c67c53
joyyuanxm <850974686@qq.com>
2016-03-26 03:59:44 +0800

    除样式和删除外所有功能已经写好

--- res/js/main.js
+++ res/js/main.js
@@ -1,24 +1,24 @@
 var pageNumber;
 var peopleList;
-var obj1;
-var obj2;
+var obj3;
 var text;
-
+var emailId;
+var detailId;
+var deleteId;
 $(document).ready(function () {
     personGet();
+    returnToM();
+    $('.informationGroup').hide();
+    $('#returnToM').hide();
 });
 function personGet() {
     $.get("http://localhost/cyhn2/users", {
         page: 0
     }, function (result) {
-         obj1 = JSON.parse(result);
-        obj2=obj1.data;
-        var obj3=obj2.count;
-        for( var i=0;i<2;i++){
-            text=obj2+"."+i;
-            alert(text);
-        }
-    var t = "";
+        var obj1 = JSON.parse(result);
+        var obj2 = obj1.data;
+        peopleList = obj2.users;
+        var t = "";
         t += '<table data-toggle="table" data-side-pagination="server" data-pagination="true" data-page-list="[5,10,20,50]">';
         t += " <tr>" +
             "<td >姓名</td>" +
@@ -32,7 +32,6 @@ function personGet() {
             "<td>操作</td>" +
             "<td>删除</td>" +
             "</tr>";
-
         for (var i = 0; i < peopleList.length; i++) {
             t += "<tr >";
             t += "<div>";
@@ -45,6 +44,7 @@ function personGet() {
             t += "<td>" + peopleList[i].projectArea + "</td>";
             t += "<td>" + "<div class='detail'></div>" + "</td>";
             t += "<td>" + "<div class='sendEmail'></div>" + "</td>";
+            t += "<td>" + "<div class='delete'></div>" + "</td>";
             t += "</div>";
             t += "</tr>";
         }
@@ -56,15 +56,76 @@ function personGet() {
         $('.sendEmail').each(function (e) {
             $(this).attr('id', 'sendEmail' + e);
         });
+        $('.delete').each(function (h) {
+            $(this).attr('id', 'delete' + h);
+        });
         for (var k = 0; k < peopleList.length; k++) {
             var button = document.createElement("button");
-            button.id = peopleList[k].Id;
+            button.id = 'email' + peopleList[k].Id;
             button.innerHTML = '发送报名表';
+            (function (k) {
+                button.addEventListener("click", function (f) {
+                    emailId = peopleList[k].Id;
+                    emailGive();
+                }, false);
+            })(k);
             $('#sendEmail' + k).append(button);
             var aDetail = document.createElement("a");
-            aDetail.id = peopleList[k].Id;
+            aDetail.id = 'a' + peopleList[k].Id;
             aDetail.innerHTML = '详情';
+            (function (k) {
+                aDetail.addEventListener("click", function (g) {
+                    detailId = peopleList[k].Id;
+                    detailShow();
+                }, false);
+            })(k);
             $('#detail' + k).append(aDetail);
+            var delBtn = document.createElement("img");
+            delBtn.id = 'del' + peopleList[k].Id;
+            (function (k) {
+                delBtn.addEventListener("click", function (d) {
+                    deleteId = peopleList[k].Id;
+                    tableDelete();
+                }, false);
+            })(k);
+            $('#delete' + k).append(delBtn);
         }
     });
+}
+function emailGive() {
+    $.post("http://localhost/cyhn2/users/" + emailId + "/email", function () {
+        alert("邮件已经发送成功！")
+    });
+}
+function detailShow() {
+    $.get("http://localhost/cyhn2/users/" + detailId, function (res) {
+        $('.informationGroup').show();
+        $('#returnToM').show();
+        $('#tableList').hide();
+        var pro = JSON.parse(res);
+        var detailList = pro.data;
+        $('#name').val(detailList.name);
+        $('#phone').val(detailList.phone);
+        $('#email').val(detailList.email);
+        $('#school').val(detailList.school);
+        $('#city').val(detailList.city);
+        $('#itemName').val(detailList.projectName);
+        $('#direction').val(detailList.projectArea);
+        $('#productionStage').val(detailList.projectInfo);
+        $('#introduction').val(detailList.projectStatus);
+        $('#ifFinance').val(detailList.projectIfCost);
+        $('#amountFormer').val(detailList.projectCosted);
+        $('#amountFuture').val(detailList.projectCost);
+        returnToM();
+    });
+}
+function returnToM() {
+    $('#returnToM').click(function () {
+        $('.informationGroup').hide();
+        $('#tableList').show();
+        $('#returnToM').hide();
+    });
+}
+function tableDelete() {
+
 }
